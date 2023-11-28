99639afb8841e0e44c114254a4d62e87e624087e
vijayakumar58 <vijayalingam58@gmail.com>
2023-11-28 19:48:20 +0530

    add

--- src/context/globalContext.js
+++ src/context/globalContext.js
@@ -2,7 +2,8 @@ import React, { useContext, useState } from "react"
 import axios from 'axios'
 
 
-const BASE_URL = "http://localhost:5000/api/v1/";
+// const BASE_URL = "http://localhost:5000/api/v1/";
+const BASE_URL = "https://moneymanager-backend.onrender.com"
 
 
 const GlobalContext = React.createContext()
